03936c96ae651990b0ae24f86f75eb03bfad02f5
sayal00 <sayal.chitrakar00@gmail.com>
2022-11-16 16:33:12 +0545

    basic crud operation without database

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Controller,Get } from '@nestjs/common';
+import { Controller,Get,Post,Body,Param,Delete,Patch} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
-import {Task} from "./task.model";
+import {Task,TaskStatus} from "./task.model";
+import {CreateTasksDto} from "./create-tasks.dto";
 
 @Controller('tasks')
 export class TasksController {
@@ -8,12 +9,24 @@ export class TasksController {
     constructor(private taskService : TasksService){
 
     }
-
     @Get()
     getAllTasks(): Task[]{
         return this.taskService.getAllTasks();
     }
-
-
-
+    @Get('/:id')
+    getTask (@Param('id') id:string):Task{
+       return this.taskService.getTask(id);
+    }
+    @Post()
+    createTask(@Body() createTaskDto : CreateTasksDto):Task{
+       return this.taskService.createTasks(createTaskDto)
+    }
+    @Delete('/:id')
+    deleteTask(@Param('id')id:string):null{
+        return this.taskService.deleteTask(id);
+    }
+    @Patch('/:id/status')
+    updateTask(@Param('id')id:string,@Body('status') status:TaskStatus):Task{
+        return this.taskService.updateTask(id,status);
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@nestjs/common';
 import {Task, TaskStatus} from './task.model';
-import * as uuid from 'uuid';
+import {v1 as uuid} from 'uuid';
+import {CreateTasksDto} from "./create-tasks.dto";
 
 @Injectable()
 export class TasksService {
@@ -11,8 +12,12 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTasks(title:string,description:string):Task{
+    getTask(id:string):Task{
+        return this.tasks.find((task)=>task.id === id);
+    }
+     createTasks(createTaskDto:CreateTasksDto):Task{
 
+        const {title,description} = createTaskDto;
         const task:Task = {
             id:uuid(),
             title,
@@ -22,6 +27,17 @@ export class TasksService {
 
         this.tasks.push(task);
         return task;
+    }
+
+    deleteTask(id:string):null{
+
+        this.tasks = this.tasks.filter((task)=>task.id !== id);
+        return null;
+    }
+    updateTask(id:string,status:TaskStatus):Task{
+
+        this.tasks.find((task)=>task.id===id).status = status;
+        return this.tasks.find((task)=>task.id === id)
     }
 
 }
