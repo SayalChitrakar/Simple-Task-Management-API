1ddaab4d8f50abf4ffc5775dd25172d1bef26ec6
sayal00 <sayal.chitrakar00@gmail.com>
2022-11-16 14:29:38 +0545

    initial commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@nestjs/common';
+import {Task, TaskStatus} from './task.model';
+import * as uuid from 'uuid';
+
+@Injectable()
+export class TasksService {
+
+    private tasks : Task[] = [];
+
+    getAllTasks(): Task[]{
+        return this.tasks;
+    }
+
+    createTasks(title:string,description:string):Task{
+
+        const task:Task = {
+            id:uuid(),
+            title,
+            description,
+            status:TaskStatus.OPEN
+        }
+
+        this.tasks.push(task);
+        return task;
+    }
+
+}
